ffba50079d3885fc0e13f7fe4e6062519961fc7e
Jasonlee6789 <jeffersonlee@163.com>
2020-10-27 16:33:57 +0800

    I understand the picture of redux.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
-
+import "antd/dist/antd.css";
+import { Input, Button, List } from "antd";
+import store from "./store/index";
+// const data = ["8点晨会", "9点沟通需求会", "Review代码"];
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    console.log(store.getState());
+    this.state = store.getState();
+  }
   render() {
-    return <div>Hello lijing</div>;
+    return (
+      <div style={{ margin: "10px" }}>
+        <div>
+          <Input
+            placeholder={this.state.inputValue}
+            style={{ width: "250px", marginRight: "10px" }}
+          />
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{ margin: "10px", width: "300px" }}>
+          <List
+            bordered
+            dataSource={this.state.list}
+            renderItem={(item) => <List.Item>{item}</List.Item>}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/store/reducer.js
@@ -0,0 +1,7 @@
+const previousState = {
+  inputValue: "Write sth",
+  list: ["8点晨会", "9点沟通需求会", "Review代码"],
+};
+export default (state = previousState, action) => {
+  return state;
+};
