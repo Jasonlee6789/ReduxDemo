ceecbaa0bdc1d33c719120c05af2b9a85c005b45
Jasonlee6789 <jeffersonlee@163.com>
2020-10-29 10:07:15 +0800

    I practise the route:component-action-store-reducer-store-component.

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import "antd/dist/antd.css";
 import { Input, Button, List } from "antd";
 import store from "./store/index";
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from "./store/actionTypes";
+import { changeInputAction } from "./store/actionCreators";
+
 // const data = ["8点晨会", "9点沟通需求会", "Review代码"];
 class TodoList extends Component {
   constructor(props) {
     super(props);
     console.log(store.getState());
     this.state = store.getState();
+    this.changeInputValue = this.changeInputValue.bind(this);
+    this.storeChange = this.storeChange.bind(this);
+    this.clickBtn = this.clickBtn.bind(this);
+    store.subscribe(this.storeChange);
   }
   render() {
     return (
@@ -16,18 +23,54 @@ class TodoList extends Component {
           <Input
             placeholder={this.state.inputValue}
             style={{ width: "250px", marginRight: "10px" }}
+            onChange={this.changeInputValue}
+            value={this.state.inputValue}
           />
-          <Button type="primary">增加</Button>
+          <Button type="primary" onClick={this.clickBtn}>
+            增加
+          </Button>
         </div>
         <div style={{ margin: "10px", width: "300px" }}>
           <List
             bordered
             dataSource={this.state.list}
-            renderItem={(item) => <List.Item>{item}</List.Item>}
+            renderItem={(item, index) => (
+              <List.Item onClick={this.deleteItem.bind(this, index)}>
+                {item}
+              </List.Item>
+            )}
           />
         </div>
       </div>
     );
+  }
+  changeInputValue(e) {
+    console.log(e.target.value);
+    const action = {
+      type: CHANGE_INPUT,
+      value: e.target.value,
+    };
+    store.dispatch(action);
+  }
+
+  storeChange() {
+    this.setState(store.getState());
+  }
+
+  clickBtn() {
+    const action = {
+      type: ADD_ITEM,
+    };
+    store.dispatch(action);
+  }
+
+  deleteItem(index) {
+    console.log(index);
+    const action = {
+      type: DELETE_ITEM,
+      index,
+    };
+    store.dispatch(action);
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,29 @@
+import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from "./actionTypes";
 const previousState = {
   inputValue: "Write sth",
   list: ["8点晨会", "9点沟通需求会", "Review代码"],
 };
 export default (state = previousState, action) => {
+  console.log(state, action);
+
+  if (action.type === CHANGE_INPUT) {
+    //注意reducer只能接收state,不能改变state,下面深拷贝出来一个对象
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.value;
+    return newState;
+  }
+
+  if (action.type === ADD_ITEM) {
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.list.push(newState.inputValue);
+    newState.inputValue = "";
+    return newState;
+  }
+  if (action.type === DELETE_ITEM) {
+    let newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index, 1);
+    newState.inputValue = "";
+    return newState;
+  }
   return state;
 };
