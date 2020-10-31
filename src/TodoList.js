463f512af53fd2e5a055d09b96140977336e22d4
Jasonlee6789 <jeffersonlee@163.com>
2020-10-31 10:05:12 +0800

    我完成了UI和业务逻辑 的分离.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import "antd/dist/antd.css";
-import { Input, Button, List } from "antd";
+import TodoListUI from "./TodoListUI";
 import store from "./store/index";
 // import { CHANGE_INPUT, ADD_ITEM, DELETE_ITEM } from "./store/actionTypes";
 import {
@@ -18,34 +17,17 @@ class TodoList extends Component {
     this.changeInputValue = this.changeInputValue.bind(this);
     this.storeChange = this.storeChange.bind(this);
     this.clickBtn = this.clickBtn.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
     store.subscribe(this.storeChange);
   }
   render() {
     return (
-      <div style={{ margin: "10px" }}>
-        <div>
-          <Input
-            placeholder={this.state.inputValue}
-            style={{ width: "250px", marginRight: "10px" }}
-            onChange={this.changeInputValue}
-            value={this.state.inputValue}
-          />
-          <Button type="primary" onClick={this.clickBtn}>
-            增加
-          </Button>
-        </div>
-        <div style={{ margin: "10px", width: "300px" }}>
-          <List
-            bordered
-            dataSource={this.state.list}
-            renderItem={(item, index) => (
-              <List.Item onClick={this.deleteItem.bind(this, index)}>
-                {item}
-              </List.Item>
-            )}
-          />
-        </div>
-      </div>
+      <TodoListUI
+        inputValue={this.state.inputValue}
+        changeInputValue={this.changeInputValue}
+        clickBtn={this.clickBtn}
+        list={this.state.list}
+      />
     );
   }
   changeInputValue(e) {
