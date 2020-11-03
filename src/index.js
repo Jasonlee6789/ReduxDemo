81e776d9ae2a5d3f6bd6accc2980638784caf1d2
Jasonlee6789 <jeffersonlee@163.com>
2020-11-03 10:42:54 +0800

    我开始练习用useReducer和useContext来替代Redux.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TodoList from "./TodoList";
+import ReducerDemo from "./ReducerDemo";
 
-ReactDOM.render(<TodoList />, document.getElementById("root"));
+ReactDOM.render(<ReducerDemo />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
