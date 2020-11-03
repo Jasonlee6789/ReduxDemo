322ed8360a4edc23392a0b8761fef27cbbfae2cb
Jasonlee6789 <jeffersonlee@163.com>
2020-11-03 18:23:55 +0800

    I practise the useContext and useReducer to replace the Redux.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import TodoList from "./TodoList";
 import ReducerDemo from "./ReducerDemo";
+import Example from "./example6/Example6";
 
-ReactDOM.render(<ReducerDemo />, document.getElementById("root"));
+ReactDOM.render(<Example />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
