7bd038497b9df6eb462cfb0f7b5fc643c611ce83
Jasonlee6789 <jeffersonlee@163.com>
2020-11-04 10:45:36 +0800

    我用自定义的函数来串一遍hooks的基本应用。

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from "react-dom";
 // import ReducerDemo from "./ReducerDemo";
 // import Example from "./example6/Example6";
 // import Example from "./useMemoDemo";
-import useRefDemo from "./useRefDemo";
+// import useRefDemo from "./useRefDemo";
+import Example from "./Example9";
 
-ReactDOM.render(<useRefDemo />, document.getElementById("root"));
+ReactDOM.render(<Example />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
